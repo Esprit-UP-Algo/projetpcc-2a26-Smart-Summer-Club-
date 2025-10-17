8f0048d04df4a189577f78b3b417bcca5b20f51f
yasminehidri1 <yasmine.hidri@esprit.tn>
2025-10-17 16:18:58 +0100

    interface +sql s6

---
+++ Desktop/projetpcc-2a26-vibraclub-integration/projetpcc-2a26-vibraclub-integration/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/debug/moc_employeradmin.cpp
@@ -0,0 +1,312 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'employeradmin.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.7.3)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../employeradmin.h"
+#include <QtGui/qtextcursor.h>
+#include <QtCore/qmetatype.h>
+
+#include <QtCore/qtmochelpers.h>
+
+#include <memory>
+
+
+#include <QtCore/qxptype_traits.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'employeradmin.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.7.3. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSEmployerAdminENDCLASS_t {};
+constexpr auto qt_meta_stringdata_CLASSEmployerAdminENDCLASS = QtMocHelpers::stringData(
+    "EmployerAdmin",
+    "onNavigationButtonClicked",
+    "",
+    "onSearchEmployees",
+    "onUploadPhoto",
+    "onConfirmAdd",
+    "onConfirmUpdate",
+    "populateEmployeeTable",
+    "populateStatistics",
+    "onEditEmployee",
+    "row",
+    "onDeleteEmployee",
+    "onEditMember",
+    "onDeleteMember",
+    "onEditActivity",
+    "onDeleteActivity",
+    "onEquipmentDetails",
+    "equipmentData",
+    "onEquipmentManage",
+    "onEquipmentDelete",
+    "onAddPayment",
+    "onClearPaymentForm",
+    "onPaymentSearchTextChanged",
+    "onEditPayment",
+    "paymentData",
+    "onDeletePayment",
+    "onSortEmployees",
+    "onExportEmployees",
+    "onSortMembers",
+    "onExportMembers",
+    "onSortActivities",
+    "index",
+    "onExportActivities",
+    "onSortEquipment",
+    "onExportEquipment"
+);
+#else  // !QT_MOC_HAS_STRINGDATA
+#error "qtmochelpers.h not found or too old."
+#endif // !QT_MOC_HAS_STRINGDATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSEmployerAdminENDCLASS[] = {
+
+ // content:
+      12,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      29,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,  188,    2, 0x08,    1 /* Private */,
+       3,    0,  189,    2, 0x08,    2 /* Private */,
+       4,    0,  190,    2, 0x08,    3 /* Private */,
+       5,    0,  191,    2, 0x08,    4 /* Private */,
+       6,    0,  192,    2, 0x08,    5 /* Private */,
+       7,    0,  193,    2, 0x08,    6 /* Private */,
+       8,    0,  194,    2, 0x08,    7 /* Private */,
+       9,    1,  195,    2, 0x08,    8 /* Private */,
+      11,    1,  198,    2, 0x08,   10 /* Private */,
+      12,    1,  201,    2, 0x08,   12 /* Private */,
+      13,    1,  204,    2, 0x08,   14 /* Private */,
+      14,    1,  207,    2, 0x08,   16 /* Private */,
+      15,    1,  210,    2, 0x08,   18 /* Private */,
+      16,    1,  213,    2, 0x08,   20 /* Private */,
+      18,    1,  216,    2, 0x08,   22 /* Private */,
+      19,    2,  219,    2, 0x08,   24 /* Private */,
+      20,    0,  224,    2, 0x08,   27 /* Private */,
+      21,    0,  225,    2, 0x08,   28 /* Private */,
+      22,    0,  226,    2, 0x08,   29 /* Private */,
+      23,    1,  227,    2, 0x08,   30 /* Private */,
+      25,    2,  230,    2, 0x08,   32 /* Private */,
+      26,    0,  235,    2, 0x08,   35 /* Private */,
+      27,    0,  236,    2, 0x08,   36 /* Private */,
+      28,    0,  237,    2, 0x08,   37 /* Private */,
+      29,    0,  238,    2, 0x08,   38 /* Private */,
+      30,    1,  239,    2, 0x08,   39 /* Private */,
+      32,    0,  242,    2, 0x08,   41 /* Private */,
+      33,    0,  243,    2, 0x08,   42 /* Private */,
+      34,    0,  244,    2, 0x08,   43 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   10,
+    QMetaType::Void, QMetaType::Int,   10,
+    QMetaType::Void, QMetaType::Int,   10,
+    QMetaType::Void, QMetaType::Int,   10,
+    QMetaType::Void, QMetaType::Int,   10,
+    QMetaType::Void, QMetaType::Int,   10,
+    QMetaType::Void, QMetaType::QStringList,   17,
+    QMetaType::Void, QMetaType::QStringList,   17,
+    QMetaType::Void, QMetaType::QStringList, QMetaType::Int,   17,   10,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QStringList,   24,
+    QMetaType::Void, QMetaType::QStringList, QMetaType::Int,   24,   10,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   31,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject EmployerAdmin::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_CLASSEmployerAdminENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSEmployerAdminENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSEmployerAdminENDCLASS_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<EmployerAdmin, std::true_type>,
+        // method 'onNavigationButtonClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'onSearchEmployees'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'onUploadPhoto'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'onConfirmAdd'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'onConfirmUpdate'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'populateEmployeeTable'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'populateStatistics'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'onEditEmployee'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'onDeleteEmployee'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'onEditMember'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'onDeleteMember'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'onEditActivity'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'onDeleteActivity'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'onEquipmentDetails'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QStringList &, std::false_type>,
+        // method 'onEquipmentManage'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QStringList &, std::false_type>,
+        // method 'onEquipmentDelete'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QStringList &, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'onAddPayment'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'onClearPaymentForm'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'onPaymentSearchTextChanged'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'onEditPayment'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QStringList &, std::false_type>,
+        // method 'onDeletePayment'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QStringList &, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'onSortEmployees'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'onExportEmployees'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'onSortMembers'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'onExportMembers'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'onSortActivities'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'onExportActivities'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'onSortEquipment'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'onExportEquipment'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void EmployerAdmin::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<EmployerAdmin *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->onNavigationButtonClicked(); break;
+        case 1: _t->onSearchEmployees(); break;
+        case 2: _t->onUploadPhoto(); break;
+        case 3: _t->onConfirmAdd(); break;
+        case 4: _t->onConfirmUpdate(); break;
+        case 5: _t->populateEmployeeTable(); break;
+        case 6: _t->populateStatistics(); break;
+        case 7: _t->onEditEmployee((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 8: _t->onDeleteEmployee((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 9: _t->onEditMember((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 10: _t->onDeleteMember((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 11: _t->onEditActivity((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 12: _t->onDeleteActivity((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 13: _t->onEquipmentDetails((*reinterpret_cast< std::add_pointer_t<QStringList>>(_a[1]))); break;
+        case 14: _t->onEquipmentManage((*reinterpret_cast< std::add_pointer_t<QStringList>>(_a[1]))); break;
+        case 15: _t->onEquipmentDelete((*reinterpret_cast< std::add_pointer_t<QStringList>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<int>>(_a[2]))); break;
+        case 16: _t->onAddPayment(); break;
+        case 17: _t->onClearPaymentForm(); break;
+        case 18: _t->onPaymentSearchTextChanged(); break;
+        case 19: _t->onEditPayment((*reinterpret_cast< std::add_pointer_t<QStringList>>(_a[1]))); break;
+        case 20: _t->onDeletePayment((*reinterpret_cast< std::add_pointer_t<QStringList>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<int>>(_a[2]))); break;
+        case 21: _t->onSortEmployees(); break;
+        case 22: _t->onExportEmployees(); break;
+        case 23: _t->onSortMembers(); break;
+        case 24: _t->onExportMembers(); break;
+        case 25: _t->onSortActivities((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 26: _t->onExportActivities(); break;
+        case 27: _t->onSortEquipment(); break;
+        case 28: _t->onExportEquipment(); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject *EmployerAdmin::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *EmployerAdmin::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSEmployerAdminENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int EmployerAdmin::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 29)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 29;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 29)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 29;
+    }
+    return _id;
+}
+QT_WARNING_POP
